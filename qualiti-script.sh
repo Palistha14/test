c46595abacced685bb0f4900abe39e4379d59782
Palistha Shakya <caroze20@gmail.com>
2021-03-17 14:43:25 +0545

    script

--- qualiti-script.sh
+++ qualiti-script.sh
@@ -8,9 +8,9 @@
     API_URL='https://7iggpnqgq9.execute-api.us-east-2.amazonaws.com/udbodh/api'
     INTEGRATION_JWT_TOKEN='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwcm9qZWN0X2lkIjozMzIsImFwaV9rZXlfaWQiOjE3ODgsIm5hbWUiOiIiLCJkZXNjcmlwdGlvbiI6IiIsImljb24iOiIiLCJpbnRlZ3JhdGlvbl9uYW1lIjoiQ2lyY2xlY2kiLCJvcHRpb25zIjp7fSwiaWF0IjoxNjE1ODAwMzg5fQ.xop8zsjCp44clIk2WdHkNxBLDV0UIAbcnlskR7TSEzY'
     INTEGRATIONS_API_URL='http://4c061b6b99c5.ngrok.io'
-    
-    apt-get update -y
-    apt-get install -y jq
+       
+    sudo apt-get update -y
+    sudo apt-get install -y jq
     
     #Trigger test run
     TEST_RUN_ID="$( \
