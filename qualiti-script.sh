38db9c52716af4c8fa027aa873c1d827a8c229f5
Palistha Shakya <caroze20@gmail.com>
2021-03-22 11:07:22 +0545

    sh file

--- qualiti-script.sh
+++ qualiti-script.sh
@@ -3,25 +3,22 @@
 set -ex
  
 PROJECT_ID='332'
-API_KEY='nKrOmTdoX44ss9YhOuJlb2fE7218lw6w65kJAHRs'
-CLIENT_ID='5b2e66ea6f2b3d6282c693cdb264cea4'
+API_KEY='N0756mWWnG7zbJD1xxXNZ4eMGAfSgIYR9rDNCKEU'
+CLIENT_ID='17a9d90862f70c761c6a3372135aaa24'
 SCOPES=['"ViewTestResults"','"ViewAutomationHistory"']
 API_URL='https://7iggpnqgq9.execute-api.us-east-2.amazonaws.com/udbodh/api'
-INTEGRATION_JWT_TOKEN='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwcm9qZWN0X2lkIjozMzIsImFwaV9rZXlfaWQiOjIyNzgsIm5hbWUiOiIiLCJkZXNjcmlwdGlvbiI6IiIsImljb24iOiIiLCJpbnRlZ3JhdGlvbl9uYW1lIjoiQ2lyY2xlY2kiLCJvcHRpb25zIjp7fSwiaWF0IjoxNjE2MDQyMjU4fQ.AgrNFifnYDUIC8Vt6wY18qJtcB_XMa51PfJnW41nY14'
-INTEGRATIONS_API_URL='http://9f960070d582.ngrok.io'
+INTEGRATION_JWT_TOKEN='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwcm9qZWN0X2lkIjozMzIsImFwaV9rZXlfaWQiOjI0ODEsIm5hbWUiOiIiLCJkZXNjcmlwdGlvbiI6IiIsImljb24iOiIiLCJpbnRlZ3JhdGlvbl9uYW1lIjoiQ2lyY2xlY2kiLCJvcHRpb25zIjp7fSwiaWF0IjoxNjE2MTQ1MzM4fQ.TO3ZA8nte9dfi6mp26mm15jHg8P1tNcJBITL4FK34EQ'
+INTEGRATIONS_API_URL='http://a76a6ecf629f.ngrok.io'
  
-# sudo apt-get update -y
-# sudo cp -p /etc/apt/sources.list{,.back}
-# sudo apt-get -o Acquire::Check-Valid-Until=false update
-sudo sed -i '/jessie-backports/d' /etc/apt/sources.list 
-sudo sed -i '/jessie-updates/d' /etc/apt/sources.list ZZ
-sudo apt-get install -y jq
+apt-get update -y
+apt-get install -y jq
  
 #Trigger test run
 TEST_RUN_ID="$( \
   curl -X POST -G ${INTEGRATIONS_API_URL}/api/integrations/circleci/${PROJECT_ID}/events \
     -d 'token='$INTEGRATION_JWT_TOKEN''\
     -d 'triggerType=Deploy'\
+    -d 'projectId='$PROJECT_ID''\
   | jq -r '.test_run_id')"
  
 AUTHORIZATION_TOKEN="$( \
